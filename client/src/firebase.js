746d4146979f7814e65a6698be1f3c2b1c0f5f09
Sumithra <sumithrat49@gmail.com>
2025-03-26 12:41:54 +0530

    firebadse api

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -6,7 +6,7 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  // apiKey: "AIzaSyB4lrusFmpVJNTndJH2vRd0nxs8ALTSUL0",
+
   authDomain: "mern-blocks.firebaseapp.com",
   projectId: "mern-blocks",
   storageBucket: "mern-blocks.firebasestorage.app",
